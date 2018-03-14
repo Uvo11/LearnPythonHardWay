02e4845123b904a5b84ac8f595de135e1f3ecf54
Leeoku <leeoku@gmail.com>
2018-03-14 14:26:07 -0400

    Add files via upload
    exercises 1-20

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, first, second, third, fourth = argv
+
+print("The script is called:", script)
+print("Your first variable is: ", first)
+print("Your second variable is ", second)
+print("Your third variable is: ", third)
+food = input("What is your favourite food?")
+print(f"{fourth} likes to eat {food}")
