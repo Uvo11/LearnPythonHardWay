a52d514f534746b49be149ebf5952b1a6e59ae31
Leeoku <leeoku@gmail.com>
2018-03-26 20:40:09 -0400

    Added completed version of file

--- ex37.py
+++ ex37.py
@@ -1,5 +1,11 @@
 from sys import exit
 from random import *
+from sys import argv
+script = argv
+
+def introduction():
+    print (script)
+    print(f"Welcome to the {script} game!")
 
 def entrance():
     print("You see doors to your left, right and in front, which do you choose?")
@@ -41,13 +47,15 @@ def genie_room():
         print("After you pick up the sword, the entire dungeon crumbles, but you wake up in your room.")
         win("With sword in hand, you rejoice!")
     else:
-        d
+        print("So which you going to pick?")
+        genie_room()
 
 def puzzle_room():
     puzzle = randint(1,3)
     print("You see a giant board pop up with a puzzle to solve")
+    print (puzzle)
     if puzzle == 1:
-        puzzle1 = ((5+3)*2)^2
+        puzzle1 = ((5+3)*2)**2
         print(puzzle1)
         answer = input("> ")
         if answer == "256":
@@ -55,4 +63,49 @@ def puzzle_room():
             genie_room()
         else:
             dead("The board turns red and flies out hitting you in the face.")
+    if puzzle == 2:
+        puzzle2 = 0/(((3+5)**2)*3)
+        print(puzzle2)
+        answer = input("> ")
+        if answer == "0":
+            print("The board disappears and a door opens in the back of the room")
+            genie_room()
+        else:
+            dead("The board turns red and flies out hitting you in the face.")
+    if puzzle == 3:
+        puzzle3 = ((2*2)**2)**2
+        print(puzzle3)
+        answer = input("> ")
+        if answer == "256":
+            print("The board disappears and a door opens in the back of the room")
+            genie_room()
+        else:
+            dead("The board turns red and flies out hitting you in the face.")
+
+def monster_room():
+    print("You see a giant Snorlax blocking the doorway. Which option do you pick?")
+    print("Option 1: Squish behind the Snorlax")
+    print("Option 2: Poke Snorlax")
+    print("Option 3: Feed him candy.")
+    option = input("> ")
+
+    if option == "1":
+        dead("You try to move behind the Snorlax, but it wakes up and squishes you to death.")
+    elif option == "2":
+        print("You poke Snorlax's belly. It shifts abit but nothing happens. Try again!")
+        monster_room()
+    elif option == "3":
+        candy = ['chocolate','lollipop', 'cookie']
+        print(f'Which candy will you give? {candy}')
+        feed_candy = input('> ')
+        if feed_candy=="lollipop":
+            print("You give Snorlax a lollipop. It smiles and gives you a hug. You feel happy and move to the next door.")
+            genie_room()
+        else:
+            print("Snorlax spits it out...")
+            monster_room()
+    else:
+        dead("Snorlax's stomach growls and grabs you for lunch.")
+
+introduction()
 entrance()
